44677236944ab9086a7ef39b746b79c658c9a063
followersito <45855921+followersito@users.noreply.github.com>
2019-04-23 13:47:34 -0600

    Metodos para convertir a String

---
+++ app2.js
@@ -0,0 +1,14 @@
+let codigoPostal;
+
+codigoPostal = 90210;
+codigoPostal = String(codigoPostal);
+
+// Booleanos
+dato = true;
+dato = String(dato);
+console.log(dato.length);
+console.log(typeof dato);
+dato = 29;
+dato = dato.toString();
+console.log(typeof dato);
+console.log(codigoPostal.length);
