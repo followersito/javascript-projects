55ab0b121d817b8a7cfd6c6ecee3bc448ac371ec
followersito <45855921+followersito@users.noreply.github.com>
2019-04-23 12:01:42 -0600

    Métodos principales utilizados con números.

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 // Formas de crear variables: 
 // var let const
 
-let usando = "Usando",
-    tecnologia = "JavaScript";
+// let usando = "Usando",
+//     tecnologia = "JavaScript";
 
-console.log(`${usando} ${tecnologia}`);
+// console.log(`${usando} ${tecnologia}`);
+
+let dato;
+dato = Number("20");
+dato = Number('25.455');
+dato = Number(false);
+dato = Number(null);
+dato = Number(undefined);
+dato = Number([1,2,3,4,5])
+
+// ParseFloat y ParseInt
+dato = parseInt('100');
+dato = parseFloat('1512.20300');
+
+// toFixed utiliza solo los decimales enviados en parámetros
+dato = 1525.25287754; 
+dato = '151258877.6655354745';
+
+console.log(parseFloat(dato).toFixed(5));
+
