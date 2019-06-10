6717a3fd4acf9e349f46116791672502586db918
Iván Melchor <ivanmelchormejia@gmail.com>
2019-06-10 14:23:52 -0600

    Cambios en identación

--- universidad/js/eventListeners.js
+++ universidad/js/eventListeners.js
@@ -1,9 +1,12 @@
 // Event Listener de click al buscador
-document.querySelector('#submit-buscador').addEventListener('click', function(e){
+document.querySelector('#submit-buscador').addEventListener('click', function (e) {
     e.preventDefault();
     alert('Buscando...');
 });
 
-function ejecutarBoton(){
-    
-}
+// Otra forma de escribir el código de arriba 
+// document.querySelector('#submit-buscador').addEventListener('click', ejecutarBoton);
+
+// function ejecutarBoton() {
+//     preventDefault();
+// }
